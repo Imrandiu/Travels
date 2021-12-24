109257aa986e32d912bd6a5852c3b3ffe167bc3b
Imran Hossain <47759379+Imrandiu@users.noreply.github.com>
2021-12-24 00:16:09 -0800

    Add files via upload

---
+++ comment.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="fonts/css/fontawesome-all.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/media.css">
+
+        <title>Travels</title>
+
+    </head>
+    <body style="font-family: 'Playfair Display', serif;">
+       <header>
+            <div class="row wrapper navbar-fixed-top">
+                <div class="col-sm-4 ">
+                    <div class="logo">
+                        <img src="images/logo.png" alt="logo">
+                    </div>
+                </div>
+                <div class="col-sm-6 offset-sm-2  ">
+                    <div class="navber">
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="place.php">View Place</a></li>             
+                            <li><a href="comment.php">Comment</a></li>
+                            <li><a href="https://www.google.com/maps" target="_blank">Map</a></li>
+                            <li><a href="">Log_out</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
+<!--        header end----------->
+        
+        
+        <div class="container-fluid background" >
+                <div class="row">
+                    <div class="col-md-4 col-sm-4 col-xs-12"></div>
+                    <div class="col-lg-4 col-sm-4 col-xs-12 shadow">
+                        <form class="" method="get" action="logged_user.php" style="">
+                            <h1  style="color:white; font-weight:100">Add comment:</h1>
+                            <div class="form-group">
+                                <label style="color:white;" for="exampleInputEmail1">write here...</label>
+                                <textarea rows="6" cols="50" class="form-control" placeholder="comment"></textarea>
+                            </div>
+                            <button  input type="submit" class="btn btn-success btn-block">sent..</button>
+                            <br>
+                            <br>
+
+                            
+
+                            
+                        </form>
+                        
+ <h1 style="text-align: center;font-size: 32px;margin-top: 51px;color:white;">Not Register? &nbsp;<a href="registretion.php" target="_blank" class="btn btn-danger btn-rounded">Sign up!</a></h1>
+
+
+
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-12"></div>
+                </div>
+
+            </div>
+            
+          
+         <!--Footer-->
+        <footer class="page-footer font-small stylish-color-dark pt-4 mt-4">
+
+            <!--Footer Links-->
+            <div class="container text-center text-md-left">
+                <div class="row">
+
+                    <!--First column-->
+                    <div class="col-md-4">
+                        <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">About us</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ducimus enim et aspernatur, labore, beatae mollitia doloremque deserunt quo dolorem eveniet quidem quaerat veniam saepe pariatur maiores id, dicta voluptas.10</p>
+                    </div>
+                    <!--/.First column-->
+
+                    <hr class="clearfix w-100 d-md-none">
+
+                    <!--Second column-->
+                    <div class="col-md-2 mx-auto">
+                        <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="we.php">About us</a>
+                            </li>
+                            <li>
+                                <a href="#!">Blog</a>
+                            </li>
+                            <li>
+                                <a href="#!">Product for Mac</a>
+                            </li>
+                            <li>
+                                <a href="#!">Product for Windows</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--/.Second column-->
+
+                    <hr class="clearfix w-100 d-md-none">
+
+                    <!--Third column-->
+                    <div class="col-md-2 mx-auto">
+                        <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="#!">Web analytics</a>
+                            </li>
+                            <li>
+                                <a href="#!">Presentations</a>
+                            </li>
+                            <li>
+                                <a href="#!">Product Help</a>
+                            </li>
+                            <li>
+                                <a href="#!">Developer Apl</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--/.Third column-->
+
+                    <hr class="clearfix w-100 d-md-none">
+
+                    <!--Fourth column-->
+                    <div class="col-md-2 mx-auto">
+                        <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="#!">facebook</a>
+                            </li>
+                            <li>
+                                <a href="#!">twitter</a>
+                            </li>
+                            <li>
+                                <a href="#!">inatagram</a>
+                            </li>
+                            <li>
+                                <a href="#!">google+</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--/.Fourth column-->
+                </div>
+            </div>
+            <!--/.Footer Links-->
+
+            <hr>
+
+            <!--Call to action-->
+            <div class="text-center py-3">
+                <ul class="list-unstyled list-inline mb-0">
+                    <li class="list-inline-item">
+                        <h5 class="mb-1">Register for free</h5>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="registretion.php" target="_blank" class="btn btn-danger btn-rounded">Sign up!</a>
+                    </li>
+                </ul>
+            </div>
+            <!--/.Call to action-->
+
+            <hr>
+
+
+        </footer>
+        <!--/.Footer-->
+        
+
+      
+
+
+
+            
+
+            </body>
+        </html>
---
+++ help.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="fonts/css/fontawesome-all.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/media.css">
+
+        <title>Travels</title>
+
+    </head>
+
+    <body style="font-family: 'Playfair Display', serif;">
+        <header>
+            <div class="row wrapper navbar-fixed-top">
+                <div class="col-sm-4 ">
+                    <div class="logo">
+                        <img src="images/logo.png" alt="logo">
+                    </div>
+                </div>
+                <div class="col-sm-6 offset-sm-2  ">
+                    <div class="navber">
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="place.php">View Place</a></li> 
+                            <li><a href="comment.php" >Comment</a></li> 
+                            <li><a href="index.php">Log_out</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
+<!--        header end----------->
+        
+<!--        slider start--------------->
+       
+        
+    <div class="container-fluid background">
+        <div class="row">
+
+            <div class="col-md-2 col-sm-4 col-xs-12"></div>
+            <div class="col-md-8 col-sm-8 col-xs-12 shadow register_bottom_space">
+                <form class="form-container " action="" method="post">
+                    <h1 style="text-align: center; color:white;">Answer all to get advice..</h1>
+
+                    <div class="form-group">
+                        <label  style="color:white;"for="exampleInputEmail1">your tour with your ...?</label>
+                        <select name="divition">
+                           <option>Family</option>
+                           <option>Wife</option>
+                           <option>Friend's</option>
+                           <option>teachers</option>
+                        </select>
+                    </div>
+                    
+                    <div class="form-group">
+                        <label  style="color:white;"for="exampleInputEmail1">Do you like Hill?</label><br>
+                        <input style="color:white;" type="radio" value="yes" name="hill"/>Yes
+                        <input style="color:white;" type="radio" value="no" name="hill"/>No
+                    </div>
+                    <div class="form-group">
+                        <label  style="color:white;"for="exampleInputEmail1">Do you like Sea?</label><br>
+                        <input style="color:white;" type="radio" value="yes" name="hill"/>Yes
+                        <input style="color:white;" type="radio" value="no" name="hill"/>No
+                    </div>
+                    <div class="form-group">
+                        <label  style="color:white;"for="exampleInputEmail1">Do you like to see sunset & sunrise?</label><br>
+                        <input style="color:white;" type="radio" value="yes" name="hill"/>Yes
+                        <input style="color:white;" type="radio" value="no" name="hill"/>No
+                    </div>
+                    <div class="form-group">
+                        <label  style="color:white;" for="exampleInputName1">How long you want to spent?</label>
+                        <input type="text" name="ans" class="form-control" id="exampleInputName1" placeholder="">
+                    </div>
+                    <div class="form-group">
+                        <label  style="color:white;" for="exampleInputName1">what about your budget?</label>
+                        <input type="text" name="ans" class="form-control" id="exampleInputName1" placeholder="">
+                    </div>
+                    
+                    <div class="form-group">
+                        <label  style="color:white;" for="exampleInputPhone1">Anything you want</label>
+                        <input type="text" name="address" class="form-control" id="exampleInputEmail1" placeholder="about your choice">
+                    </div>
+                    
+                   <input class="submit" name="submit" type="Submit" value="Submit">
+                    <br><br>
+
+<!--                    <button input type="submit" name="submit" class="btn btn-success btn-block">Register</button>-->
+                    
+
+
+
+
+
+
+
+                </form>
+
+
+
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-12"></div>
+        </div>
+
+    </div>
+
+        
+        
+
+        <!--Footer-->
+        <footer class="page-footer font-small stylish-color-dark pt-4 mt-4">
+
+            <!--Footer Links-->
+            <div class="container text-center text-md-left">
+                <div class="row">
+
+                    <!--First column-->
+                    <div class="col-md-4">
+                        <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">About us</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ducimus enim et aspernatur, labore, beatae mollitia doloremque deserunt quo dolorem eveniet quidem quaerat veniam saepe pariatur maiores id, dicta voluptas.10</p>
+                    </div>
+                    <!--/.First column-->
+
+                    <hr class="clearfix w-100 d-md-none">
+
+                    <!--Second column-->
+                    <div class="col-md-2 mx-auto">
+                        <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="we.php">About us</a>
+                            </li>
+                            <li>
+                                <a href="#!">Blog</a>
+                            </li>
+                            <li>
+                                <a href="#!">Product for Mac</a>
+                            </li>
+                            <li>
+                                <a href="#!">Product for Windows</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--/.Second column-->
+
+                    <hr class="clearfix w-100 d-md-none">
+
+                    <!--Third column-->
+                    <div class="col-md-2 mx-auto">
+                        <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="#!">Web analytics</a>
+                            </li>
+                            <li>
+                                <a href="#!">Presentations</a>
+                            </li>
+                            <li>
+                                <a href="#!">Product Help</a>
+                            </li>
+                            <li>
+                                <a href="#!">Developer Apl</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--/.Third column-->
+
+                    <hr class="clearfix w-100 d-md-none">
+
+                    <!--Fourth column-->
+                    <div class="col-md-2 mx-auto">
+                        <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="#!">facebook</a>
+                            </li>
+                            <li>
+                                <a href="#!">twitter</a>
+                            </li>
+                            <li>
+                                <a href="#!">inatagram</a>
+                            </li>
+                            <li>
+                                <a href="#!">google+</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--/.Fourth column-->
+                </div>
+            </div>
+            <!--/.Footer Links-->
+
+            <hr>
+
+            <!--Call to action-->
+            <div class="text-center py-3">
+                <ul class="list-unstyled list-inline mb-0">
+                    <li class="list-inline-item">
+                        <h5 class="mb-1">Register for free</h5>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="registretion.php" target="_blank" class="btn btn-danger btn-rounded">Sign up!</a>
+                    </li>
+                </ul>
+            </div>
+            <!--/.Call to action-->
+
+            <hr>
+
+
+        </footer>
+        <!--/.Footer-->
+
+
+
+
+<!--       footer -->
+
+
+
+
+
+        <script src="js/jquery-3.3.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/script.js"></script>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
+
